7e742c235874ed612511a77bc121935c3ebdb710
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2020-07-31 01:45:38 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const userID = "479877128368685056";
 
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
 
-client.on("message", (message) => {
-if(!message.sender === userID)
-{
 
-if(message.content === 'ping')
-{
-    message.channel.send('pong');
-}
-}});
+client.on('message', message => {
+    if (message.content === 'ping') {
+       message.reply('pong');
+       }
+});
 
 client.login(process.env.BOT_TOKEN);
