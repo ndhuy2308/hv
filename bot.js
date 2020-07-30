49f62cea032ddd3b9115e0b5dc20e7c83d3a5054
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2020-07-31 01:16:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,26 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('CONNECTED!');
-
-});
-
-
-
-let prefix = "#";
-client.on("message", (message) => {
-  // Exit and stop if the prefix is not there or if user is a bot
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
  
-  if (message.content.startsWith(prefix + "7nung")) {
-    message.channel.send("Nung lol me thien ha!!");
-  } 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
-client.login(process.env.BOT_TOKEN);
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
