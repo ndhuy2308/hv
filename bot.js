7b5451f58442f8c591647bd6e44b6b56c59d3d0d
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2020-07-31 01:34:41 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,14 +7,14 @@ client.on('ready', () => {
 
 });
 
-client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+let prefix = "";
+client.on("message", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong");
+  } 
 });
 
 client.login(process.env.BOT_TOKEN);
